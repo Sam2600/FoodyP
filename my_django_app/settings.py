c8c1e48dda1816776d81516e1f6f199841c83ca1
Kaung Htet San <kghtetsan26@gmail.com>
2025-06-16 13:24:52 +0630

    Enhance admin interface by adding OrderItem management and refining existing model registrations

--- my_django_app/settings.py
+++ my_django_app/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'store',
     'django.contrib.admin',
     'django.contrib.auth',
--- store/admin.py
+++ store/admin.py
@@ -1,9 +1,40 @@
 from django.contrib import admin
-
 # Register your models here.
 from django.contrib import admin
-from .models import Item, Order, Category
+from .models import Item, Order, Category, OrderItem
 
-admin.site.register(Item)
-admin.site.register(Order)
-admin.site.register(Category)
+# CATEGORY ADMIN
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+# ITEM ADMIN
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category', 'created_by', 'created_at')
+    list_filter = ('category', 'created_at')
+    search_fields = ('name', 'description')
+    ordering = ('-created_at',)
+
+# INLINE ORDER ITEMS FOR ORDER ADMIN
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ('name', 'description', 'price', 'quantity', 'category', 'image_url', 'sub_total')
+    extra = 0
+
+# ORDER ADMIN
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_at', 'total_amount', 'summary')
+    search_fields = ('summary',)
+    ordering = ('-created_at',)
+    list_per_page = 20
+    inlines = [OrderItemInline]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'name', 'quantity', 'price', 'sub_total')
+    search_fields = ('name', 'order__id')
+    list_filter = ('category',)
+    ordering = ('-id',)
